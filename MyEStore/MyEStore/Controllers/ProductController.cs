66ee7a39b0fc4e5c129903260c60f7660c92ac7a
Hien Luong Tran Hy <hyhien@gmail.com>
2025-12-09 10:46:37 +0700

    url friendly

--- MyEStore/MyEStore/Controllers/ProductController.cs
+++ MyEStore/MyEStore/Controllers/ProductController.cs
@@ -27,6 +27,7 @@ namespace MyEStore.Controllers
 
 		}
 
+		[HttpGet("/san-pham/{slug}-{id}")]
 		public IActionResult Detail(int id)
 		{
 			var product = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == id);
